bbb8ef35acaa839ea995eab46d92feeef03daa8e
YulisaGR <yulisg@uw.edu>
2023-02-16 18:21:57 -0800

    add code

--- value-calculations.R
+++ value-calculations.R
@@ -1,8 +1,51 @@
 gw_data <- read.csv("global_witness_led_22-09-22.csv", stringsAsFactors = FALSE)
+library('stringr')
 
-# county with the most deaths 
-country_max <- gw_data %>% 
+# 1. What was the country with the most deaths between 2012 and 2021? 
+country_with_max_deaths <- gw_data %>% 
   group_by(country) %>% 
   summarize(total_deaths = sum(number_of_victims, na.rm = T)) %>% 
   filter(total_deaths == max(total_deaths)) %>%
   pull(country)
+# Brazil
+
+# 2. How many environmental defenders were killed in the country with the max deaths (Brazil) between 2012 and 2021?
+max_deaths_country<- gw_data %>% 
+  group_by(country) %>% 
+  summarize(total_deaths = sum(number_of_victims, na.rm = T)) %>% 
+  filter(total_deaths == max(total_deaths)) %>%
+  pull(total_deaths)
+# 342
+  
+# 3. What was the year with the most deaths worldwide?
+year_max_deaths <- gw_data %>% 
+  mutate(Year = str_sub(date, start = 1, end = 4)) %>% 
+  group_by(Year) %>% 
+  summarize(total_deaths = sum(number_of_victims, na.rm = T)) %>% 
+  filter(total_deaths == max(total_deaths)) %>%
+  pull(Year)
+# 2020
+
+# 4. What was the most common perpetrator type over the decade?
+main_perpetrator <- gw_data %>% 
+  group_by(perpetrator_type) %>% 
+  summarize(total_deaths = sum(number_of_victims, na.rm = T)) %>% 
+  filter(total_deaths == max(total_deaths)) %>%
+  pull(perpetrator_type)
+# Unspecified 
+  
+# 5. What was the biggest group of environmental defenders that were attacked?
+main_person_characteristic <- gw_data %>% 
+  group_by(person_characteristics) %>% 
+  summarize(total_deaths = sum(number_of_victims, na.rm = T)) %>% 
+  filter(total_deaths == max(total_deaths)) %>%
+  pull(person_characteristics)
+# Indigenous People
+
+# 6. What was the top industry sector environmental defenders were killed for?
+main_industry_sector <- gw_data %>% 
+  group_by(industry_driver) %>% 
+  summarize(total_deaths = sum(number_of_victims, na.rm = T)) %>% 
+  filter(total_deaths == max(total_deaths)) %>%
+  pull(industry_driver)
+# Sector could not be confirmed

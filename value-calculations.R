e1c69dc251be612ee3b37b87bf3ce626f110c315
YulisaGR <yulisg@uw.edu>
2023-02-16 18:30:42 -0800

    add values

--- value-calculations.R
+++ value-calculations.R
@@ -1,7 +1,7 @@
 gw_data <- read.csv("global_witness_led_22-09-22.csv", stringsAsFactors = FALSE)
 library('stringr')
 
-# 1. What was the country with the most deaths between 2012 and 2021? 
+# 1. Which country had the most deaths between 2012 and 2021? 
 country_with_max_deaths <- gw_data %>% 
   group_by(country) %>% 
   summarize(total_deaths = sum(number_of_victims, na.rm = T)) %>% 
@@ -9,7 +9,7 @@ country_with_max_deaths <- gw_data %>%
   pull(country)
 # Brazil
 
-# 2. How many environmental defenders were killed in the country with the max deaths (Brazil) between 2012 and 2021?
+# 2. How many environmental defenders were killed in the country with the most deaths (Brazil) between 2012 and 2021?
 max_deaths_country<- gw_data %>% 
   group_by(country) %>% 
   summarize(total_deaths = sum(number_of_victims, na.rm = T)) %>% 
@@ -26,7 +26,7 @@ year_max_deaths <- gw_data %>%
   pull(Year)
 # 2020
 
-# 4. What was the most common perpetrator type over the decade?
+# 4. What was the most common perpetrator type?
 main_perpetrator <- gw_data %>% 
   group_by(perpetrator_type) %>% 
   summarize(total_deaths = sum(number_of_victims, na.rm = T)) %>% 
@@ -34,7 +34,7 @@ main_perpetrator <- gw_data %>%
   pull(perpetrator_type)
 # Unspecified 
   
-# 5. What was the biggest group of environmental defenders that were attacked?
+# 5. What was the main personal characteristic of the environmental defenders that were attacked?
 main_person_characteristic <- gw_data %>% 
   group_by(person_characteristics) %>% 
   summarize(total_deaths = sum(number_of_victims, na.rm = T)) %>% 
@@ -42,7 +42,7 @@ main_person_characteristic <- gw_data %>%
   pull(person_characteristics)
 # Indigenous People
 
-# 6. What was the top industry sector environmental defenders were killed for?
+# 6. What was the top industry sector?
 main_industry_sector <- gw_data %>% 
   group_by(industry_driver) %>% 
   summarize(total_deaths = sum(number_of_victims, na.rm = T)) %>% 
